5062dc7e63f486d9b65c55ce239d2207eb70b85a
wilker22 <wilker.silva@codevasf.gov.br>
2022-06-09 21:39:33 -0300

    portfolio 3

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Portfolio;
-use Image;
+use Carbon\Carbon;
+use Intervention\Image\Facades\Image;
+
 
 class PortfolioController extends Controller
 {
@@ -21,6 +23,36 @@ class PortfolioController extends Controller
 
 
     public function storePortfolio(Request $request){
+        $request->validate([
+            'portfolio_name' => 'required',
+            'portfolio_title' => 'required',
+            'portfolio_image' => 'required',
 
+        ],[
+
+            'portfolio_name.required' => 'Portfolio Name is Required',
+            'portfolio_title.required' => 'Portfolio Titile is Required',
+        ]);
+
+        $image = $request->file('portfolio_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($image)->resize(1020,519)->save('upload/portfolio/'.$name_gen);
+            $save_url = 'upload/portfolio/'.$name_gen;
+
+            Portfolio::insert([
+                'portfolio_name' => $request->portfolio_name,
+                'portfolio_title' => $request->portfolio_title,
+                'portfolio_description' => $request->portfolio_description,
+                'portfolio_image' => $save_url,
+                'created_at' => Carbon::now(),
+
+            ]); 
+            $notification = array(
+            'message' => 'Portfolio Inserted Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.portfolio')->with($notification);
     }// End Method
 }
